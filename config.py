22b8771175000e73cba415a1c912bc21b97c1992
Pobeda7 <116107245+Pobeda7@users.noreply.github.com>
2022-11-25 16:33:00 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,10 @@
+token = '5911612183:AAFfFIzyDOiM6bTmi36OKhmVqLVf1o4lzj4'
+
+keys = {
+    'евро': 'EUR',
+    'доллар': 'USD',
+    'рубль' : 'RUB',
+    'Евро': 'EUR',
+    'Доллар': 'USD',
+    'Рубль': 'RUB'
+}
